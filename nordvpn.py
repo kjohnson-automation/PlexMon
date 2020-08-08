032196e5adee4e443510cb098e64eee2db0bc003
Kevin Johnson <k.johnson.automation@gmail.com>
2020-08-08 10:12:19 -0400

    VPN update
    Nord handler has been updated
    - uses os.system for single connection/disconnect
    - uses subprocess.run for server name and group connections

--- nordvpn.py
+++ nordvpn.py
@@ -1,24 +1,29 @@
 # NordVPN handler
 import subprocess
+import os
 
 
 class NordVPN():
     """ Just a basic handler for starting/stoping the VPN service """
     def __init__(self, location):
-        self.location = location
+        self.sub_location = f"{location}"
+        self.sys_location = f"\"{location}\""
+        print(f"VPN Path: {self.sub_location}")
 
     def connect(self, connect_cmd:str="-c"):
         """ Connects - using quick connect to NordVPN service """
-        subprocess.run([f"{self.location}", f"{connect_cmd}"])
+        os.system(f"\"{self.sys_location}\" -c")
 
     def disconnect(self, disconnect_cmd:str="-d"):
         """ Disconnects from NordVPN service """
-        subprocess.run([f"{self.location}", f"{disconnect_cmd}"])
+        os.system(f"{self.sys_location} {disconnect_cmd}")
         
     def connect_name(self, server_name:str="United States #4914"):
         """ Connects to <server_name> """
-        subprocess.run([f"{self.location}", f"-c -n {server_name}"])
+        cmd_list = [self.sub_location, "-c", "-n", f"{server_name}"]
+        subprocess.run(cmd_list)
 
     def connect_group(self, server_group:str="Obfuscated Servers"):
         """ Connects to the "best" server in <server_group> """
-        subprocess.run([f"{self.location}", f"-c -g {server_group}"])
+        cmd_list = [self.sub_location, "-c", "-g", f"{server_group}"]
+        subprocess.run(cmd_list)
